6aa04be1607216e804f0c5b43917f5f3ec68f058
Dexter-03 <tobias.reischmann@uni-muenster.de>
2015-01-22 13:51:00 +0100

    Cleaned the bundle list_of_open_issues - Resized the window - Deleted old files - Deleted old unused code

--- generic-md2-bundles/md2_list_of_open_issues/OpenIssuesListController.js
+++ generic-md2-bundles/md2_list_of_open_issues/OpenIssuesListController.js
@@ -8,93 +8,55 @@ define(["dojo/_base/declare",
     "ct/store/ComplexMemory"],
         function (declare, array, OpenIssuesListWidget, _Connect, Stateful, DataViewModel, DataView, ComplexMemory) {
             return declare([_Connect], {
-                        i18n: {
-                            grid: {
-                                title: "Title",
-                                dataView: {
-                                    DGRID: {
-                                        noDataMessage: "No custom info(s) found..."
-                                    },
-                                    pager: {
-                                        pageSizeLabelText: "Custom info ${pageStartItemNumber}-${pageEndItemNumber} of ${itemCount}"
-                                    }
-                                }
+                i18n: {
+                    grid: {
+                        title: "Title",
+                        dataView: {
+                            DGRID: {
+                                noDataMessage: "No custom info(s) found..."
+                            },
+                            pager: {
+                                pageSizeLabelText: "Custom info ${pageStartItemNumber}-${pageEndItemNumber} of ${itemCount}"
                             }
-                        },
-                        /**
-                    * @constructs
-                    */
-                   constructor: function () {
-                       this._listeners = new _Connect();
-                   },
+                        }
+                    }
+                },
+                /**
+                * @constructs
+                */
+                constructor: function () {
+                    this._listeners = new _Connect();
+                },
                 createInstance: function () {
-                  //  var mapModel = this._mapModel;
-                    //get all service nodes from map model
-                  //  var serviceNodes = mapModel.getServiceNodes();
-                  //  var glassNodes = mapModel.getGlassPaneLayer().children;
-                  //  array.forEach(glassNodes, function(node){
-                  //      serviceNodes.push(node);
-                   // });
-                    var widget = new OpenIssuesListWidget({
-                 //       nodes: serviceNodes
-                    nodes: [
-                    new Stateful({
-                        id: 1,
-                        workflowElement: "MediaCapturing",
-                        lastEventFired: "LocationDetectedEvent",
-                        waitingSince: "2015-01-14"
-                    }),
-                    new Stateful({
-                        id: 2,
-                        workflowElement: "LocationDetection",
-                        lastEventFired: "ComplaintSubmitEvent",
-                        waitingSince: "2015-01-12"
-                    }),
-                    new Stateful({
-                        id: 3,
-                        workflowElement: "MediaCapturing",
-                        lastEventFired: "LocationDetectedEvent",
-                        waitingSince: "2015-01-13"
-                    })
-                    ]
-                    });
-                    //connect to event and force reload
-                  //  this.connect(widget,"onLayerUpdated", this._updateLayer);
-                    //return widget;
                     return this.build_dataView();
                 },
                 destroyInstance: function (widget) {
                     this.disconnect();
                     widget.destroyRecursive();
                 },
-               // _updateLayer : function() {
-               //     this._mapModel.fireModelNodeStateChanged({
-               //         source: this
-               //     });
-               // }
-               build_dataView: function(){
-                   var store = new ComplexMemory({
-                    idProperty: "id",
-                    data: [
-                    new Stateful({
-                        id: 1,
-                        workflowElement: "MediaCapturing",
-                        lastEventFired: "LocationDetectedEvent",
-                        waitingSince: "2015-01-14"
-                    }),
-                    new Stateful({
-                        id: 2,
-                        workflowElement: "LocationDetection",
-                        lastEventFired: "ComplaintSubmitEvent",
-                        waitingSince: "2015-01-12"
-                    }),
-                    new Stateful({
-                        id: 3,
-                        workflowElement: "MediaCapturing",
-                        lastEventFired: "LocationDetectedEvent",
-                        waitingSince: "2015-01-13"
-                    })
-                    ]
+                build_dataView: function(){
+                    var store = new ComplexMemory({
+                        idProperty: "id",
+                        data: [
+                        new Stateful({
+                            id: 1,
+                            workflowElement: "MediaCapturing",
+                            lastEventFired: "LocationDetectedEvent",
+                            waitingSince: "2015-01-14"
+                        }),
+                        new Stateful({
+                            id: 2,
+                            workflowElement: "LocationDetection",
+                            lastEventFired: "ComplaintSubmitEvent",
+                            waitingSince: "2015-01-12"
+                        }),
+                        new Stateful({
+                            id: 3,
+                            workflowElement: "MediaCapturing",
+                            lastEventFired: "LocationDetectedEvent",
+                            waitingSince: "2015-01-13"
+                        })
+                        ]
                     });
                     store.getMetadata = function(){
                         return {
@@ -122,45 +84,68 @@ define(["dojo/_base/declare",
                             }]
                         };
                     };
-                   var model = this._viewModel = new DataViewModel({
-                    store: store
-                });
-                var dataView = this._dataView = this._createDataView();
-                //this._gridNode.set("content", dataView);
-                dataView.startup();
-                dataView.set("model", model);
-                this._listeners.connect(dataView, "onItemClicked", this, function (evt) {
-                    //this.editCustomInfo(evt.itemId);
-                    alert("You clicked on " +  evt.itemId);
-                });
-                return dataView;
-               },
-               _createDataView: function(){
-                 var i18n = this.i18n.grid.dataView;
-            var dataView = this._dataView = new DataView({
-                i18n: i18n,
-                showFilter: true,
-                filterDuringKeyUp: true,
-                showPager: true,
-                showViewButtons: false,
-                itemsPerPage: 10,
-                DGRID: {
-                    noDataMessage: i18n.noDataFound,
-                    checkboxSelection: false/*,
-                    columns: [
-                        {
-                            matches: {
-                                name: {
-                                    $eq: "id"
+                    var model = this._viewModel = new DataViewModel({
+                        store: store
+                    });
+                    var dataView = this._dataView = this._createDataView();
+                    //this._gridNode.set("content", dataView);
+                    dataView.startup();
+                    dataView.set("model", model);
+                    this._listeners.connect(dataView, "onItemClicked", this, function (evt) {
+                        //this.editCustomInfo(evt.itemId);
+                        alert("You clicked on " +  evt.itemId);
+                    });
+                    return dataView;
+                },
+                _createDataView: function(){
+                    var i18n = this.i18n.grid.dataView;
+                    var dataView = this._dataView = new DataView({
+                        i18n: i18n,
+                        showFilter: true,
+                        filterDuringKeyUp: true,
+                        showPager: true,
+                        showViewButtons: false,
+                        itemsPerPage: 10,
+                        DGRID: {
+                            noDataMessage: i18n.noDataFound,
+                            checkboxSelection: false,
+                            columns: [//can filter the columns to be shown
+                                /*{
+                                    "matches": {
+                                      "name": "id"
+                                    },
+                                      "title": "ID"
+                                },*/
+                                {
+                                    "matches": {
+                                      "name": "workflowElement"
+                                    },
+                                      "title": "Workflow Element"
+                                },
+                                {
+                                    "matches": {
+                                      "name": "lastEventFired"
+                                    },
+                                      "title": "Last Event Fired"
+                                },
+                                {
+                                    "matches": {
+                                      "name": "waitingSince"
+                                    },
+                                      "title": "Waiting Since"
                                 }
-                            }
+                                /*,{ //Maybe use this for generic column filtering
+                                    matches: {
+                                        name: {
+                                            $eq: "id"
+                                        }
+                                    }
+                                }*/
+                            ]
                         }
-                    ]*/
+                    });
+                    return dataView;
                 }
-            });
-            //dataView.setToolbar(this.gridToolbar);
-            return dataView;
-               }
                
             });
         });
